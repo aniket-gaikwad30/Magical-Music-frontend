8e3f76044eb5f042ab24d6fb78c0f4951854e011
aniket-gaikwad30 <aniketgaikwadhere30@gmail.com>
2026-02-22 20:39:09 +0530

    changes

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5050/api" : "/api",
+	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5050/api" : "https://magical-music-backend-production.up.railway.app/api",
 });
